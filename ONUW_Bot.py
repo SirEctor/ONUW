94aed895bab4b597c0becf2b2e3305efc1580136
Ebenezer Teshome <ebenezer.teshome@duke.edu>
2020-09-29 08:49:06 -0400

    webbrowser:

--- ONUW_Bot.py
+++ ONUW_Bot.py
@@ -1,4 +1,11 @@
-import webbrowser
+import webbrowser, bs4, requests
+from selenium import webdriver
+if __name__ == "__main__":
+    print("")
+    #credit - https://stackoverflow.com/questions/832331/launch-a-webpage-on-a-firefox-win-tab-using-python
+    firefox = webbrowser.Mozilla("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
 
-if __name__ == "main":
-    webbrowser.open("https://werewolf.invades.space/")
+    print("\nReady to Join!")
+    joinCode = input("Enter join code:\n")
+    firefox.open_new_tab("https://werewolf.invades.space/")
+
